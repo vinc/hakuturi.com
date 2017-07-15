e10acda17b953adcd3dd12d83a86d5c707b9a853
Vincent Ollivier <v@vinc.cc>
2017-07-15 16:39:39 +0200

    Install pundit

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
 
   def index
     @links = Link.all.order(created_at: :desc)
@@ -10,13 +11,16 @@ class LinksController < ApplicationController
 
   def new
     @link = Link.new
+    authorize @link
   end
 
   def edit
+    authorize @link
   end
 
   def create
     @link = Link.new(link_params)
+    authorize @link
 
     respond_to do |format|
       if @link.save
@@ -30,6 +34,7 @@ class LinksController < ApplicationController
   end
 
   def update
+    authorize @link
     respond_to do |format|
       if @link.update(link_params)
         format.html { redirect_to root_url, notice: 'Link was successfully updated.' }
@@ -42,6 +47,7 @@ class LinksController < ApplicationController
   end
 
   def destroy
+    authorize @link
     @link.destroy
     respond_to do |format|
       format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
---
+++ app/policies/link_policy.rb
@@ -0,0 +1,13 @@
+class LinkPolicy < ApplicationPolicy
+  def create?
+    user.present? && user.admin?
+  end
+
+  def update?
+    user.present? && user.admin?
+  end
+
+  def destroy?
+    user.present? && user.admin?
+  end
+end
