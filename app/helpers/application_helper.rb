836a64056779c7b5a69f77ce4e336a12e7c1fd6d
Vincent Ollivier <v@vinc.cc>
2018-04-22 16:12:39 +0200

    Add meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def page_meta_tags
+    display_meta_tags(
+      site: "Hakituri News",
+      separator: "-",
+      reverse: true,
+      canonical: url_for(only_path: false, locale: nil),
+      og: {
+        title: :title,
+        site_name: :site,
+        url: :canonical
+      }
+    )
+  end
 end
