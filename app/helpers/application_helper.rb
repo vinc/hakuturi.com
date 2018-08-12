579ad2e5b7479bf300fd36db0f3be8ff49b782c3
Vincent Ollivier <v@vinc.cc>
2018-08-12 14:02:45 +0200

    Rename Hakituri to Hākuturi

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def page_meta_tags
     display_meta_tags(
-      site: "Hakituri News",
+      site: "Hākuturi News",
       separator: "-",
       reverse: true,
       canonical: url_for(only_path: false, locale: nil),
