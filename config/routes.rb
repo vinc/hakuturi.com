57cb2341759d627c951195f44eb96fdbde98f8e0
Vincent Ollivier <v@vinc.cc>
2018-08-15 13:31:48 +0200

    Update theme for signed in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :links, path: "news"
   resources :notes
+
+  scope(path_names: { new: "add" }) do
+    resources :links, path: "news"
+  end
 
   root "home#index" # to: redirect("/news")
 end
