f5a56ca043973c971450f7d4c667db2e6e649ff1
Vincent Ollivier <v@vinc.cc>
2018-08-15 17:19:16 +0200

    Fix redirect link after save

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -30,7 +30,7 @@ class LinksController < ApplicationController
 
     respond_to do |format|
       if @link.save
-        format.html { redirect_to root_url, notice: "Link was successfully created." }
+        format.html { redirect_to links_path, notice: "Link was successfully created." }
         format.json { render :show, status: :created, location: @link }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class LinksController < ApplicationController
     authorize @link
     respond_to do |format|
       if @link.update(link_params)
-        format.html { redirect_to root_url, notice: "Link was successfully updated." }
+        format.html { redirect_to links_path, notice: "Link was successfully updated." }
         format.json { render :show, status: :ok, location: @link }
       else
         format.html { render :edit }
